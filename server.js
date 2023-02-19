00d1e5f777319d6f66083f9ce7993a8dc3129af8
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-02-19 13:49:43 +0530

    CHG | Create Home Route

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 8000;
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Routes
+app.get("/",(req, res) => {
+    res.send("Home Page");
+});
 
 //Connect to DB and start Server
+const PORT = process.env.PORT || 8000;
+
 mongoose.set("strictQuery", false);
 mongoose
     .connect(process.env.MONGO_URI)
