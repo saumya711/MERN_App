c06ff447004f28ee4209e168d9383cadbf0a9cf5
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-03-05 21:33:51 +0530

    CHG | Login User Function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
 }
 
+// Register User
 const registerUser = asyncHandler( async (req, res) => {
     const { name, email, password } = req.body
 
@@ -62,8 +63,53 @@ const registerUser = asyncHandler( async (req, res) => {
         res.status(400)
         throw new Error("Invalid user data")
     }
+});
+
+// Login User
+const loginUser = asyncHandler( async (req, res) => {
+    const {email, password} = req.body
+
+    //Validate Request
+    if(!email || !password) {
+        res.status(400)
+        throw new Error("Please add email  and password");
+    }
+
+    // Check if user exists
+    const user = await User.findOne({email})
+    if(!user) {
+        res.status(400)
+        throw new Error("User not exists, Please SignUp");
+    }
+
+    // User exists, Check if password is correct
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    // Generate Token
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400), // 1 day
+        sameSite: "none",
+        secure: true
+    });
+
+    if(user && passwordIsCorrect) {
+        const { _id, name, email, photo, phone, bio } = user
+        res.status(201).json({
+            _id, name, email, photo, phone, bio, token,
+        })
+    }else{
+        res.status(400)
+        throw new Error("Invalid email or password");
+    }
+
 })
 
 module.exports = {
     registerUser,
+    loginUser
 }
