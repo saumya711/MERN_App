f227859e826796bb0232af5c69a8d4161d77b692
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-03-06 09:51:04 +0530

    CHG | Logout User Function

--- controllers/userController.js
+++ controllers/userController.js
@@ -109,7 +109,21 @@ const loginUser = asyncHandler( async (req, res) => {
 
 })
 
+// Logout User
+const logout = asyncHandler (async ( req, res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0), 
+        sameSite: "none",
+        secure: true
+    });
+    return res.status(200).json({ message: "Successfully Logged Out" })
+
+})
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    logout
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController");
+const { registerUser, loginUser, logout } = require("../controllers/userController");
 
 router.post("/register", registerUser);
-router.post("/login", loginUser)
+router.post("/login", loginUser);
+
+router.get("/logout", logout);
 
 module.exports = router;
