0c96d5be83a93076200e72ed3707fa5da4f2a562
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-03-13 07:33:53 +0530

    CHG | Create Login status Funtion

--- controllers/userController.js
+++ controllers/userController.js
@@ -150,11 +150,28 @@ const getUser = asyncHandler(async (req, res) => {
       res.status(400);
       throw new Error("User Not Found");
     }
-  });
+});
+
+// Get Login Status
+const loginStatus = asyncHandler(async (req, res) => {
+
+    const token = req.cookies.token;
+    if(!token){
+        return res.json(false);
+    }
+    
+    // Verify Token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if(verified) {
+        return res.json(true);
+    }
+    res.json(false);
+});
 
 module.exports = {
     registerUser,
     loginUser,
     logout,
-    getUser
+    getUser,
+    loginStatus
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, logout, getUser } = require("../controllers/userController");
+const { registerUser, loginUser, logout, getUser, loginStatus } = require("../controllers/userController");
 const protect = require("../middleWare/authMiddleware");
 
 router.post("/register", registerUser);
@@ -8,5 +8,6 @@ router.post("/login", loginUser);
 
 router.get("/logout", logout);
 router.get("/get-user", protect, getUser);
+router.get("/loggedin", loginStatus);
 
 module.exports = router;
